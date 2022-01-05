49f8dd29008d16ca8384aa60f22084b9571d3556
k-yamaki <k.yamaki@hiroshimamm.com>
2022-01-05 10:43:15 +0900

    Initial Commit

---
+++ Sample60-FlowButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample60-FlowButton
+//
+//  Created by keiji yamaki on 2022/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample60-FlowButton/Sample60_FlowButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample60_FlowButtonApp.swift
+//  Sample60-FlowButton
+//
+//  Created by keiji yamaki on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct Sample60_FlowButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
